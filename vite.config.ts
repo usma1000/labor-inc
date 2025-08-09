ce88b4274d9323dd6444a41e82920fdf142ac2ec
usma1000 <usma1000@gmail.com>
2025-08-08 17:54:42 -0700

    Add gh-pages dependency and update deploy script in package.json; ensure asset linking in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./", // Ensures assets are correctly linked in production
   plugins: [react(), tailwindcss()],
 });
